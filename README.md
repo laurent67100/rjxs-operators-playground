f98ea794fda7f0ab4ee38d7ea559aed6885bf89e
laurent67100 <lvillemin@gmail.com>
2019-05-28 10:12:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dwza1y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dwza1y)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+interface operatorCatergory {
+  name: string;
+  operators: string[];
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+
+  currentOperator: string;
+
+  operators: operatorCatergory[] = [
+    {
+      name: 'Creation',
+      operators: ['of', 'from', 'fromEvent']
+    },
+     {
+      name: 'Utility',
+      operators: ['tap', 'from', 'fromEvent']
+    }
+  ];
+
+}
